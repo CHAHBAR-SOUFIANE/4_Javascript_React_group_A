60438935514355233b00caec94278b707e9c1584
CHAHBAR-SOUFIANE <chahbar.soufiane.dev@gmail.com>
2021-03-22 14:51:11 +0100

    Ajoutation des exemples de Numbres,Les chaînes de caractères,Les variables

---
+++ prereque/script.js
@@ -0,0 +1,115 @@
+// Numbres
+Math.sin(3.5);
+console.log(Math.sin(-3.5));
+
+
+var s = "123";
+var n = 4;
+console.log(s+n);
+console.log(parseInt(s)+n);
+console.log(+s+n);
+
+var t;
+console.log(isNaN(t));
+
+// Les chaînes de caractères
+var v = "fgsi ztdyzyxf zugdzg";
+console.log("la longueur et : "+ v.length);
+
+// Les variables
+var x = 1;
+let y = 1;
+
+if (x==1) {
+  var x = 2;
+  let y = 2;
+//   console.log("dans le bloc"+ y);
+}
+console.log(x);
+// expected output: 2
+console.log(y);
+// expected output: 1
+
+// const
+const number = 42;
+number = 60;
+console.log(number);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
